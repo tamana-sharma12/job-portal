b77120c9e8e6dbbe7e3344437e7441cc1035aa2b
tamana-sharma12 <tamanasharma863@gmail.com>
2026-09-09 12:05:06 +0530

    Update backend env configuration

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://tamanasharma863_db_user:abc_123@cluster0.jx6nslm.mongodb.net/jobportal");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB Connected");
     } catch (error) {
         console.log("DB Connection Error:", error);
